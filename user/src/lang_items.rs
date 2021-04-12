429d13610c34c1f9db8419dda72c30965e66607e
Joeyscat <zhouyu.gg@qq.com>
2021-04-12 21:19:27 +0800

    规范

--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -3,7 +3,7 @@ fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
     let err = panic_info.message().unwrap();
     if let Some(location) = panic_info.location() {
         println!(
-            "Panicded at {}:{} {}",
+            "Panicked at {}:{}, {}",
             location.file(),
             location.line(),
             err
